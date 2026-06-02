52b5b421a61bd318a392ec951cdedd7e28a36891
math992452 <math992452@gmail.com>
2026-06-02 20:10:17 +0800

    Test custom reset icon

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = "sigman-cache-v11";
+const CACHE_NAME = "sigman-cache-v12";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./app.html",
-  "./styles.css?v=11",
-  "./app.js?v=11",
-  "./landing.js?v=11",
-  "./manifest.json?v=11",
+  "./styles.css?v=12",
+  "./app.js?v=12",
+  "./landing.js?v=12",
+  "./manifest.json?v=12",
   "./assets/icons/feature-protocol.svg",
   "./assets/icons/feature-wins.svg",
   "./assets/icons/feature-standard.svg",
